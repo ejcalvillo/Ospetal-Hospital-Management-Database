3dd5bb374d37660dc005530ad74a860f64e39c9f
Roman Rendon <Roman.Rendon123@gmail.com>
2025-04-16 14:11:12 -0500

    added query sql file, added Services table correctly, changed multiple table entries, and fixed the employee table

--- Ospetal.sql
+++ Ospetal.sql
@@ -2,12 +2,12 @@ CREATE DATABASE IF NOT EXISTS ospetal;
 USE ospetal;
 
 CREATE TABLE IF NOT EXISTS `patients` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `patient_id` mediumint(8) unsigned NOT NULL auto_increment,
   `first_name` varchar(255) default NULL,
   `last_name` varchar(255) default NULL,
   `address` varchar(255) default NULL,
   `birthdate` varchar(255),
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`patient_id`)
 ) AUTO_INCREMENT=1;
 
 INSERT INTO `patients` (`first_name`,`last_name`,`address`,`birthdate`)
@@ -41,68 +41,13 @@ VALUES
   SELECT * FROM patients;
 
 
-  -- Makes Service Catalog table --
-  -- More efficent than singular  `services` table --
-CREATE TABLE IF NOT EXISTS `ServiceCatalogs`(
-	Service_Code INT AUTO_INCREMENT PRIMARY KEY,
-    Service_description VARCHAR(255) NOT NULL,
-    Service_Unit_Cost DECIMAL(10,2) NOT NULL
-);
-
--- Inserts values into ServiceCatalog --
-INSERT INTO `ServiceCatalogs` (Service_Description, Service_Unit_Cost)
-VALUES
-    ('Blood Test', 25.00),
-    ('X-Ray', 150.00),
-    ('MRI Scan', 500.00),
-    ('CT Scan', 450.00),
-    ('IV Drip', 75.00),
-    ('Flu Shot', 40.00),
-    ('COVID-19 Test', 100.00),
-    ('Stitches', 80.00),
-    ('Wound Cleaning', 35.00),
-    ('Physical Therapy Session', 65.00),
-    ('Vaccination - Tetanus', 45.00),
-    ('Glucose Test', 30.00),
-    ('Ultrasound', 220.00),
-    ('Cholesterol Panel', 70.00),
-    ('Dental Cleaning', 110.00),
-    ('Vision Test', 25.00),
-    ('Skin Biopsy', 180.00),
-    ('EKG', 90.00),
-    ('Suture Removal', 50.00),
-    ('Hearing Test', 55.00),
-    ('Allergy Test', 120.00),
-    ('Nebulizer Treatment', 45.00),
-    ('Inhalation Therapy', 60.00),
-    ('Dermatology Consultation', 130.00),
-    ('Blood Pressure Monitoring', 20.00);
-
-  
-  
--- Makes the service table --  
-CREATE TABLE `AppointmentServices` (
-    Service_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Appointment_ID INT,
-    Service_Code INT,
-    Service_Quantity INT DEFAULT 1,
-    TotalCost DECIMAL(10,2) GENERATED ALWAYS AS (
-        Service_Quantity * (
-            SELECT Service_Unit_Cost 
-            FROM ServiceCatalog 
-            WHERE ServiceCatalog.Service_Code = Service.Service_Code
-        )
-    ) STORED,
-    FOREIGN KEY (Appointment_ID) REFERENCES Appointment(Appointment_ID),
-    FOREIGN KEY (Service_Code) REFERENCES ServiceCatalog(Service_Code)
-);
 
 -- Department Table (Has Department ID and Name)
 CREATE TABLE IF NOT EXISTS `department` (
   `department_id` mediumint(8) unsigned NOT NULL auto_increment,
   `department_name` varchar(255),
   PRIMARY KEY (`department_id`)
-) AUTO_INCREMENT=1000;
+) AUTO_INCREMENT=1;
 
 INSERT INTO `department` (`department_name`)
 VALUES
@@ -124,147 +69,162 @@ VALUES
 
 SELECT * FROM department; 
 
--- Table for Appointment
-CREATE TABLE IF NOT EXISTS `appointment` (
+
+
+create TABLE IF NOT EXISTS `Employees`(
+  `Employee_ID` mediumint(8) unsigned NOT NULL auto_increment,
+  `Department_ID` mediumint(8) unsigned,
+  `first_name` varchar(255) default NULL,
+  `last_name` varchar(255) default NULL,
+  `email` VARCHAR(150) UNIQUE,
+  `hire_date` DATE,
+  `exit_date` DATE,
+  `Salary` FLOAT NOT NULL,
+  PRIMARY KEY (`employee_ID`),
+  FOREIGN KEY (`Department_ID`) REFERENCES department(`department_id`)
+) AUTO_INCREMENT=10000;
+
+INSERT INTO Employees
+  (Employee_ID, Department_ID, first_name, last_name, email, hire_date, exit_date, Salary)
+VALUES
+  (101, 1, 'Len', 'Graham', 'lee.Graham@email.com',
+       '2023-09-08', NULL, 146500),
+  (102, 2, 'Drake', 'Dale', 'Drake.Dale@gmail.com',
+       '2023-06-15', '2020-01-01', 80000),
+  (103, 3, 'kane', 'Miranda', 'Kane.Miranda@gmail.com',
+       '2022-02-05', NULL, 90000),
+  (104, 4, 'Katell', 'Vazquez', 'Katell.Vazquez@gmail.com',
+       '2023-05-16', NULL, 100000),
+  (105, 5, 'Ella', 'Best', 'varius.orci@aol.couk',
+       '2024-02-15', NULL, 75000),
+  (106, 6, 'Ieoma', 'sellers', 'pretium.neque@icloud.com',
+       '2024-01-01', '2020-02-15', 81000),
+  (107, 7, 'deborah', 'blake', 'summernielsen2106@google.com',
+       '2023-05-19', NULL, 75000),
+  (108, 8, 'Yen', 'welson', 'lareinakline3989@aol.couk',
+       '2023-05-20', NULL, 110000),
+  (109, 9, 'Marvin', 'Spear', 'rahimclemons6030@protonmail.net',
+       '2023-05-19', '2020-03-10', 150000),
+  (110, 10, 'Herman', 'Maddox', 'ardenvalentine@protonmail.ca',
+       '2021-05-01', NULL, 160000),
+  (111, 11, 'dona', 'Griffith', 'myratravis5112@yahoo.net',
+       '2021-05-16', NULL, 210000),
+  (112, 12, 'Piper', 'Snyder', 'imperdiet.ullamcorper.duis@hotmail.net',
+       '2020-04-12', NULL, 180000),
+  (113, 13, 'Josiah', 'Eaton', 'varius.orci123@aol.couk',
+       '2024-07-14', '2020-04-05', 200000),
+  (114, 14, 'Nasim', 'Bird', 'summernielsen210632@google.com',
+       '2019-10-29', '2020-05-20', 196800),
+  (115, 15, 'Erasmus', 'Mendez', 'ut.sem.nulla@yahoo.couk',
+       '2018-05-15', '2020-06-25', 1780000),
+  (116, 10, 'Kuame', 'Mckinney', 'pretium.neque234@icloud.com',
+       '2021-05-20', '2020-07-30', 90000),
+  (117, 9, 'Gillian', 'Sears', 'Gilliam.Sears@gmail.com',
+       '2021-04-20', '2020-08-15', 100000),
+  (118, 8, 'Wayne', 'Jarvis', 'ultrices.vivamus@protonmail.edu',
+       '2016-05-29', '2020-09-12', 95000),
+  (119, 5, 'Victoria', 'Gilmore', 'congue.in@icloud.com',
+       '2015-08-08', '2020-10-08', 200000),
+  (120, 1, 'Nicholas', 'Mclean', 'commodo.auctor@icloud.com',
+       '2020-08-06', '2020-11-05', 120000),
+  (121, 1, 'Chastity', 'Owen', 'tempus.non@hotmail.edu',
+       '2024-08-09', NULL, 140000),
+  (122, 2, 'Mara', 'Hancock', 'Mara.Hancock@hotmail.com',
+       '2023-08-08', '2020-12-31', 85000),
+  (123, 1, 'Ray', 'Fuentes', 'Ray.Fuentes@gmail.com',
+       '2022-05-19', NULL, 80000),
+  (124, 6, 'Daniel', 'sheppard', 'arcu.vel@icloud.org',
+       '2016-08-18', '2022-01-15', 70000),
+  (125, 4, 'Odysseus', 'Austin', 'nibh.sit.amet@yahoo.net',
+       '2016-08-05', NULL, 65000),
+  (126, 5, 'Daniel', 'Brock', 'egestas@outlook.net',
+       '2016-08-18', '2022-02-10', 72100),
+  (127, 8, 'Kira', 'yamato', 'GundamStrike@gmail.com',
+       '2010-05-01', NULL, 250000),
+  (128, 3, 'Lacus', 'Clyne', 'PeaceGundam@gmail.com',
+       '2024-01-01', '2022-03-20', 160000),
+  (129, 10, 'oliver', 'queen', 'GreenArrow@icloud.com',
+       '2020-05-30', '2022-04-17', 175164),
+  (130, 2, 'Madara', 'uchiha', 'spartan1980@hotmail.com',
+       '2021-03-25', '2022-05-22', 75640);
+select * FROM Employees;
+
+
+    -- Table for Appointment
+CREATE TABLE IF NOT EXISTS `appointments` (
   `appointment_id` mediumint(8) unsigned NOT NULL auto_increment,
   `employee_id` mediumint(8) unsigned NOT NULL,
   `patient_id` mediumint(8) unsigned NOT NULL,
   `diagnosis` varchar(255),
   `time` varchar(255) NOT NULL,
   `date` varchar(255) NOT NULL,
-  PRIMARY KEY (`appointment_id`)
-  -- FOREIGN KEY(`employee_id`) REFERENCES employee(`employee_id`),
-  -- FOREIGN KEY(`patient_id`) REFERENCES patient(`patient_id`)
+  PRIMARY KEY (`appointment_id`),
+  FOREIGN KEY(`employee_id`) REFERENCES Employees(`employee_id`),
+  FOREIGN KEY(`patient_id`) REFERENCES patients(`patient_id`)
 ) AUTO_INCREMENT=10000;
 
-INSERT INTO `appointment` (`employee_id`, `patient_id`, `diagnosis`, `time`, `date`)
+INSERT INTO `appointments` (`employee_id`, `patient_id`, `diagnosis`, `time`, `date`)
 VALUES
-  (118, 12, 'Influenza', '11:23 AM', '07/15/2015'),
-  (107, 5, 'Lung Cancer', '02:30 PM', '05/20/2017'),
-  (116, 18, 'Migraine', '03:15 PM', '09/12/2016'),
-  (112, 3, 'Cold', '10:45 AM', '01/05/2021'),
-  (101, 10, 'High Blood Pressure', '08:25 AM', '03/18/2019'),
-  (120, 10, 'Diabetes', '12:10 PM', '08/23/2022'),
-  (107, 14, 'Asthma', '01:50 PM', '02/14/2020'),
-  (115, 22, 'Heart Disease', '09:00 AM', '11/01/2019'),
-  (104, 8, 'Flu', '02:00 PM', '04/30/2021'),
-  (109, 1, 'Anxiety', '11:23 AM', '06/19/2020'),
-  (104, 13, 'Cough', '02:30 PM', '10/07/2021'),
-  (109, 12, 'Back Pain', '03:30 PM', '12/12/2022'),
-  (114, 13, 'Allergies', '04:05 PM', '05/09/2022'),
-  (108, 5, 'Hypertension', '08:20 AM', '11/22/2023'),
-  (105, 17, 'Inflammation', '07:55 AM', '04/18/2024'),
-  (119, 2, 'Sinus Infection', '09:10 AM', '03/02/2020'),
-  (111, 4, 'Arthritis', '01:45 PM', '08/12/2021'),
-  (111, 19, 'Bronchitis', '10:05 AM', '10/10/2020'),
-  (119, 2, 'Strep Throat', '02:50 PM', '09/25/2023'),
-  (117, 15, 'Tonsillitis', '11:40 AM', '05/14/2022');
-
-SELECT * FROM appointment;
+  (101, 12, 'Influenza', '11:23 AM', '07/15/2015'),
+  (105, 5, 'Lung Cancer', '02:30 PM', '05/20/2017'),
+  (104, 18, 'Migraine', '03:15 PM', '09/12/2016'),
+  (121, 3, 'Cold', '10:45 AM', '01/05/2021'),
+  (103, 10, 'High Blood Pressure', '08:25 AM', '03/18/2019'),
+  (107, 10, 'Diabetes', '12:10 PM', '08/23/2022'),
+  (108, 14, 'Asthma', '01:50 PM', '02/14/2020'),
+  (103, 22, 'Heart Disease', '09:00 AM', '11/01/2019'),
+  (123, 8, 'Flu', '02:00 PM', '04/30/2021'),
+  (125, 1, 'Anxiety', '11:23 AM', '06/19/2020'),
+  (127, 13, 'Cough', '02:30 PM', '10/07/2021'),
+  (112, 12, 'Back Pain', '03:30 PM', '12/12/2022'),
+  (101, 13, 'Allergies', '04:05 PM', '05/09/2022'),
+  (103, 5, 'Hypertension', '08:20 AM', '11/22/2023'),
+  (111, 17, 'Inflammation', '07:55 AM', '04/18/2024'),
+  (101, 2, 'Sinus Infection', '09:10 AM', '03/02/2020'),
+  (110, 4, 'Arthritis', '01:45 PM', '08/12/2021'),
+  (108, 19, 'Bronchitis', '10:05 AM', '10/10/2020'),
+  (121, 2, 'Strep Throat', '02:50 PM', '09/25/2023'),
+  (123, 15, 'Tonsillitis', '11:40 AM', '05/14/2022');
 
 
-CREATE TABLE IF NOT EXISTS `Employees` (
-  `Employee_ID` mediumint(8) unsigned NOT NULL auto_increment,
-  `Departmnet_ID` INT,
-  `first_name` varchar(255) default NULL,
-  `last_name` varchar(255) default NULL,
-	`email` VARCHAR(150) UNIQUE,
-  `Salary` FLOAT NOT NULL,
-  PRIMARY KEY (`employee_ID`),
-  FOREIGN KEY (department_id) REFERENCES (department_id)
-  FOREIGN KEY(`departmnet_ID`) REFERENCES employees(`employee_ID`)
-) AUTO_INCREMENT=10000;
+SELECT * FROM appointments;
 
-	insert into `Employees`(`Employee_ID`, `Department_ID`, `email`, `hire_date`, `exist_date`,`salary`)
+DROP TABLE IF EXISTS `Services`;
+-- Makes the service table --  
+CREATE TABLE `Services` (
+    Service_ID INT AUTO_INCREMENT PRIMARY KEY,
+    appointment_ID MEDIUMINT(8) UNSIGNED NOT NULL,
+    Description TEXT,  
+    UnitCost DECIMAL(10,2),      
+    Quantity INT DEFAULT 1,
+    FOREIGN KEY (appointment_ID) REFERENCES appointments(appointment_ID)
+);
 
-	values
-	(101,1, Len,Graham,'lee.Graham@email.com', 2023-09-08, 2025-12-19,146500),
-	(102,2,Drake,Dale,'Drake.Dale@gmail.com',2023-06-15,null,80000),
-    (103,3,kane, Miranda,'Kane.Miranda@gmail.com',2022-02-05,2025-10-01,90000),
-    (104,4,Katell,Vazquez,'Katell.Vazquez@gmail.com',2023-05-16,2025-10-02,100000),
-    (105,5,Ella,Best,'varius.orci@aol.couk',2024-02-15,2024-12-16,75000),
-    (106,6,Ieoma,sellers,'pretium.neque@icloud.com',2024-01-01,2024-12-31,81000),
-    (107,7,deborah,blake,'summernielsen2106@google.com',2023-05-19,2025-04-11,75000),
-    (108,8,Yen,welson,'lareinakline3989@aol.couk',2023-05-20,2025-04-11,110000),
-    (109,9,Marvin,Spear,'rahimclemons6030@protonmail.net',2023-05-19,null,150000),
-    (110,10,Herman,Maddox,'ardenvalentine@protonmail.ca',2021-05-01,null,160000),
-	(111,11,dona,Griffith,'myratravis5112@yahoo.net',2021-05-16,2025-04-11,210000),
-    (112,12,Piper ,Snyder,'imperdiet.ullamcorper.duis@hotmail.net',2020-04-12,2025-04-11,180000),
-    (113,13,Josiah,Eaton,'varius.orci@aol.couk', 2024-07-14,2025-04-03,200000),
-    (114,14,Nasim ,Bird,'summernielsen2106@google.com', 2019-10-29,2025-04-11,196,800),
-    (115,15,Erasmus ,Mendez,'ut.sem.nulla@yahoo.couk',2018-05-15,2019-05-06,178,0000),
-    (116,16,Kuame ,Mckinney,'pretium.neque@icloud.com',20217-05-20,2020-08-23,90000),
-    (117,17,Gillian ,Sears, 'Gilliam,.Sears@gmail.com',20217-04-20,2020-08-23,100000),
-    (118,18,Wayne ,Jarvis, 'ultrices.vivamus@protonmail.edu',2016-05-29,2020-08-30,95000),
-    (119,19,Victoria ,Gilmore, 'congue.in@icloud.com',2015-08-08,20217-08-28,200000),
-    (120,20,Nicholas ,Mclean, 'commodo.auctor@icloud.com',2020-08-06,2021-07-08,120000),
-    (121,1,Chastity ,Owen,'tempus.non@hotmail.edu',2024-08-09,2025-01-31,140000),
-    (122,2,Mara, Hancock,'Mara.Hancock@hotmail.com',2023-08-08,2025-01-01,85000),
-    (123,1,Ray ,Fuentes,'Ray.Fuentes@gmail.com',2022-05-19,2023-01-01,80000),
-    (124,6,Daniel ,sheppard,'arcu.vel@icloud.org',2016-08-18,204-01-01,70000),
-    (125,4,Odysseus ,Austin,'nibh.sit.amet@yahoo.net',2016-08-05,2020-05-09,65000),
-    (126,5,Daniel ,Brock,'egestas@outlook.net',2016-08-18,2021-08-18,72100),
-    (127,8,Kira,yamato,'GundamStrike@gmail.com',2010-05-01,2025-04-10,250000),
-    (128,18,Lacus,Clyne,'PeaceGundam@gmail.com',2024-01-01,2025-04-11,160000),
-    (129,14,oliver,queen,'GreenArrow@icloud.com',2020-05-30,2025-01-31,175164),
-    (130,12,Madara,uchiha,'spartan1980@hotmail.com',2021-03-25,2024-12-31,75640);
+INSERT INTO Services (Appointment_ID, Description, UnitCost, Quantity)
+VALUES
+  (10000, 'Patient with Influenza: I recommend bed rest, increased fluid intake, and consideration of antiviral medications if the patient is at risk of complications. Monitor temperature and oxygen saturation closely.', 100.00, 1),
+  (10001, 'Patient with Lung Cancer: I advise a multi-modality treatment approach including consultation with oncology for possible chemotherapy, radiation therapy, and/or surgical evaluation. Early intervention is crucial.', 2000.00, 1),
+  (10002, 'Patient with Migraine: I suggest initiating treatment with appropriate pain relievers and preventive medications, along with lifestyle modifications to reduce triggers. Follow-up is recommended if symptoms persist.', 150.00, 1),
+  (10003, 'Patient with Cold: I recommend supportive care including hydration, rest, and over-the-counter decongestants. The patient should monitor symptoms and return if the condition worsens.', 50.00, 1),
+  (10004, 'Patient with High Blood Pressure: I emphasize the importance of lifestyle modifications, dietary adjustments, and potentially starting or titrating antihypertensive medications to maintain optimal blood pressure levels.', 120.00, 1),
+  (10005, 'Patient with Diabetes: I suggest a coordinated approach involving nutrition counseling, regular blood sugar monitoring, and appropriate medication management to control glycemic levels effectively.', 180.00, 1),
+  (10006, 'Patient with Asthma: I recommend inhaled bronchodilators and corticosteroids, along with a review of trigger avoidance strategies and regular monitoring of peak flow measurements.', 75.00, 1),
+  (10007, 'Patient with Heart Disease: I propose a comprehensive management plan that includes lifestyle modifications, medication adherence, and possibly further interventional cardiology evaluation based on stress testing results.', 500.00, 1),
+  (10008, 'Patient with Flu: I advise symptomatic treatment with rest, fluids, and monitoring for any respiratory distress. Consider antiviral medication if indicated by risk factors.', 65.00, 1),
+  (10009, 'Patient with Anxiety: I recommend a combined approach of cognitive behavioral therapy and, if necessary, short-term pharmacological interventions while monitoring for improvement in symptoms.', 90.00, 1),
+  (10010, 'Patient with Cough: I suggest supportive care with cough suppressants and hydration, and I will evaluate further if the cough persists or worsens.', 55.00, 1),
+  (10011, 'Patient with Back Pain: I recommend initiating physical therapy and conservative management with NSAIDs, with imaging studies considered if the pain does not improve over time.', 200.00, 1),
+  (10012, 'Patient with Allergies: I advise antihistamines and avoidance of known allergens, and in certain cases, immunotherapy may be considered if symptoms are severe.', 80.00, 1),
+  (10013, 'Patient with Hypertension: I emphasize continued blood pressure monitoring and adjusting medication as needed along with sustained lifestyle modifications for long-term control.', 110.00, 1),
+  (10014, 'Patient with Inflammation: I propose a regimen of anti-inflammatory medications coupled with possible physical therapy; I will monitor inflammatory markers and reassess treatment based on response.', 130.00, 1),
+  (10015, 'Patient with Sinus Infection: I advise initiating an appropriate course of antibiotics along with decongestants and nasal saline irrigation to relieve symptoms and clear the infection.', 85.00, 1),
+  (10016, 'Patient with Arthritis: I recommend a tailored management plan including pain management with NSAIDs, physical therapy, and if needed, referral to a specialist for further evaluation and treatment.', 140.00, 1),
+  (10017, 'Patient with Bronchitis: I suggest supportive care with rest, inhaled bronchodilators, and maintaining hydration, with follow-up to ensure resolution of respiratory symptoms.', 95.00, 1),
+  (10018, 'Patient with Strep Throat: I recommend prompt administration of antibiotics as per current guidelines along with supportive measures such as hydration and rest to reduce symptom severity.', 70.00, 1),
+  (10019, 'Patient with Tonsillitis: I advise evaluation for recurrent cases and a combination of antibiotic therapy with supportive care. In cases of chronic recurrence, surgical options may be discussed.', 115.00, 1);
+  
+  SELECT * FROM Services
 
-    drop table employees;
-    /*(1) write a query that lits alls the employees in the table.
-    (2) write e a query thats lists all the employees with a salary greater then 100,000 
-    (3) Write a query that lists all the employees salary that is the highest the medium and the lowest.
-    (4) Write a query that lists what department the employees are assigned too*/
-    
-    SELECT Employee_ID, Department_ID, first_name,Last_name,hire_date,exit_date,Salary
-	FROM employees;
 
-    SELECT Employee_ID,Departmnet_ID,First_name,Last_name,Salary
-    from employees
-    where salary > 100000;
-    
-    select Employee_ID,
-	CASE 
-		WHEN salary > 150000 THEN 'High' 
-		WHEN salary BETWEEN 80000 AND 100000 THEN 'Medium' 
-		ELSE 'Low' 
-	END AS salary
-	FROM employees;
-    
-    select E.employees.first_name,E.employees.last_name,D.department_name
-    from employees E
-    join departments D
-    on E.department_ID = D.department_ID;
-    
-    
-    
-SELECT * FROM appointment
-ORDER BY patient_id;
-
-DROP TABLE appointment;
-
---4 Query that shows which lists diagnosis and patient_id in sorted order
-SELECT patient_id, diagnosis
-FROM appointment
-ORDER BY patient_id;
-
---5 Query that shows patient's name and diagnosis
-SELECT 
-  patients.first_name,
-  patients.last_name,
-  appointment.diagnosis
-FROM 
-  patients
-JOIN 
-  appointment ON patients.id = appointment.patient_id;
-
---6 Query that shows the time patient was appointed into the hospital
-SELECT 
-  patients.first_name,
-  patients.last_name,
-  appointment.time
-FROM 
-  patients
-JOIN 
-  appointment ON patients.id = appointment.patient_id;
 
 
